8945a9d0d6e398c810cf11b7866618f59cce45fd
Bentroen <bemcdc@gmail.com>
2022-11-16 19:48:16 -0300

    Remover dados de anos anteriores a 1990

--- convert.R
+++ convert.R
@@ -1,4 +1,4 @@
-setcwd("./PraticaDePesquisa")
+setwd("./PraticaDePesquisa")
 
 filenames <- c(
   "Environment_Emissions_by_Sector_E_All_Data",
@@ -9,13 +9,26 @@ filenames <- c(
   "Environment_LandUse_E_Flags"
 )
 
+# Create output folder
+if (!dir.exists(file.path(getwd(), "rdata"))) {
+  dir.create(file.path(getwd(), "rdata"))
+}
+
 for (f in filenames) {
   infile = paste("./data/", f, ".csv", sep="", collapse=NULL)
   outfile = paste("./rdata/", f, ".RData", sep="", collapse=NULL)
   
   file_data = read.table(infile, header=TRUE, sep=",")
+  
+  for (year in 1960:1989) {
+    col = sprintf("Y%s", year)
+    col_f = paste(col, "F", sep="")
+    file_data[[col]] <- NULL
+    file_data[[col_f]] <- NULL
+    head(file_data)
+  }
+
   save(file_data, file=outfile, compress=TRUE)
+  
 }
-
-
 
